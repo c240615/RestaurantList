59b59ae83483c2a4480469199230ee9e1d8e45a2
claire <claire210401@gmail.com>
2023-02-10 20:21:57 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
+// 進入登入頁
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+// 提交登入資料
+router.post("/login", (req, res) => {});
+// 進入註冊頁
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
-router.get('/login',(req,res)=>{
-  res.render('login')
-})
 module.exports = router;
